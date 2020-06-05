051527e39b1fcc26bdcdd8b69b818171cec29e96
yvaineChen <43847169+yvaineChen@users.noreply.github.com>
2020-06-05 09:53:21 +0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,11 +9,6 @@ public class Main {
 
     public static void main(String args[]) {
         System.out.println(new Main().sayHello());
-
-        Base64 base64 = new Base64();
-        byte[] bytes = base64.encode("I Love Gradle".getBytes());
-
-        System.out.println(new String(bytes));
     }
 
     /**
